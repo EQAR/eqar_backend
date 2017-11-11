ddde5fa09420e07b35df0d77cf6dd63d50d86411
JoshBone <jozsef.bone@gmail.com>
2017-11-11 12:14:48 +0100

    [Fix #7] Updated settings for static files	.

--- eqar_backend/settings_components/production.py
+++ eqar_backend/settings_components/production.py
@@ -4,6 +4,8 @@ import os
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from eqar_backend.settings_components.application import BASE_DIR
+
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -15,3 +17,10 @@ ALLOWED_HOSTS = []
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
