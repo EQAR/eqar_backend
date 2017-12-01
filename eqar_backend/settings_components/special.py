4574820796b036ff23e8e0c9526b3bc836aff1ab
JoshBone <jozsef.bone@gmail.com>
2017-12-01 11:29:10 +0100

    [Fix #19] TokenAuthentication is the default authentication method.

--- eqar_backend/settings_components/special.py
+++ eqar_backend/settings_components/special.py
@@ -1,7 +1,10 @@
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 }
--- eqar_backend/urls.py
+++ eqar_backend/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from rest_framework.authtoken import views
 
 urlpatterns = [
     url(r'^api/', include('discovery_api.urls', namespace='discovery_api')),
     url(r'^admin/', admin.site.urls),
+    url(r'^api-token-auth/', views.obtain_auth_token)
 ]
