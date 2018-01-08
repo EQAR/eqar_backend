bf205b541ce5a3dcea4988e6d21cafdd7d22f5f2
JoshBone <jozsef.bone@gmail.com>
2018-01-08 20:46:46 +0100

    Fix for the user admin site.

--- eqar_backend/settings_components/suit.py
+++ eqar_backend/settings_components/suit.py
@@ -1,5 +1,10 @@
 SUIT_CONFIG = {
     'ADMIN_NAME': 'DEQAR Administration',
-    'LIST_PER_PAGE': 15
+    'LIST_PER_PAGE': 15,
+    'SEARCH_URL': '/admin/auth/user/',
+    'MENU_ICONS': {
+       'sites': 'icon-leaf',
+       'auth': 'icon-lock',
+    },
 }
 
