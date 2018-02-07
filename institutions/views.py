a8ef08ee33d940ef83067d08c1897bf4ed717343
JoshBone <jozsef.bone@gmail.com>
2018-02-07 20:58:00 +0100

    Remove not used SyncETERView.

--- institutions/views.py
+++ institutions/views.py
@@ -1,15 +1,3 @@
 import json
 
 import requests
-from rest_framework.views import APIView
-
-
-class SyncETERView(APIView):
-    def get(self, request, format=None):
-        """
-        Sync ETER entries with DEQAR.
-        """
-
-        payload = {"BAS.REFYEAR.v": {"$in": [2014]}}
-        r = requests.post('https://www.eter-project.com/api/eter/HEIs/filter', data=json.dumps(payload))
-        r.status_code
