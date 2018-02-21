08b86c66851dc9ed21c9519fee88f7dd185529a2
JoshBone <jozsef.bone@gmail.com>
2018-02-21 16:34:50 +0100

    Celery settings

--- eqar_backend/settings_components/production.py
+++ eqar_backend/settings_components/production.py
@@ -20,3 +20,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'public')
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+# CELERY STUFF
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Brussels'
