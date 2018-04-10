326317c720cc7cf5eca8e92ba9017d1291c66d24
JoshBone <jozsef.bone@gmail.com>
2018-04-10 22:15:58 +0200

    Added hierarchical relationship info to the report display.

--- webapi/serializers/report_serializers.py
+++ webapi/serializers/report_serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+from institutions.models import Institution
 from reports.models import Report, ReportFile
 
 
@@ -14,12 +16,36 @@ class ReportSerializer(serializers.ModelSerializer):
     agency_url = serializers.HyperlinkedRelatedField(read_only=True, view_name="webapi-v1:agency-detail",
                                                      source='agency')
     agency_name = serializers.StringRelatedField(source='agency')
+    agency_esg_activity = serializers.SlugRelatedField(slug_field='activity', read_only=True)
     report_files = ReportFileSerializer(many=True, read_only=True, source='reportfile_set')
     status = serializers.StringRelatedField()
     decision = serializers.StringRelatedField()
     flag = serializers.StringRelatedField()
+    institution_relationship_context = serializers.SerializerMethodField()
+
+    def get_institution_relationship_context(self, obj):
+        related_institutions = []
+        related_institution = {}
+
+        for child_id in self.context['children']:
+            inst = Institution.objects.get(pk=child_id)
+            if obj.institutions.filter(pk=inst.pk).exists():
+                related_institution['id'] = inst.pk
+                related_institution['relationship'] = 'child'
+                related_institution['name_primary'] = inst.name_primary
+                related_institutions.append(related_institution)
+
+        for parent_id in self.context['parents']:
+            inst = Institution.objects.get(pk=parent_id)
+            if obj.institutions.filter(pk=inst.pk).exists():
+                related_institution['id'] = inst.pk
+                related_institution['relationship'] = 'parent'
+                related_institution['name_primary'] = inst.name_primary
+                related_institutions.append(related_institution)
+
+        return related_institutions
 
     class Meta:
         model = Report
-        fields = ['agency_name', 'agency_url', 'valid_from', 'valid_to', 'status', 'decision', 'report_files',
-                  'local_identifier', 'name', 'flag']
+        fields = ['agency_name', 'agency_url', 'agency_esg_activity', 'valid_from', 'valid_to', 'status', 'decision', 'report_files',
+                  'local_identifier', 'name', 'flag', 'institution_relationship_context']
--- webapi/views/report_views.py
+++ webapi/views/report_views.py
@@ -5,6 +5,7 @@ from django.db.models import Q
 from rest_framework import generics
 from rest_framework.filters import OrderingFilter
 
+from institutions.models import Institution
 from programmes.models import Programme
 from reports.models import Report
 from webapi.serializers.programme_serializers import ProgrammeSerializer
@@ -20,17 +21,45 @@ class ReportProgrammeListByInstitution(generics.ListAPIView):
     ordering_fields = ('name_primary',)
     ordering = ('name_primary',)
 
+    def get_serializer_context(self):
+        context = super(ReportProgrammeListByInstitution, self).get_serializer_context()
+        parents = []
+        children = []
+        institution = Institution.objects.get(pk=self.kwargs['institution'])
+
+        # Children
+        for inst in institution.relationship_parent.all():
+            children.append(inst.institution_child_id)
+
+        # Parents
+        for inst in institution.relationship_child.all():
+            parents.append(inst.institution_parent_id)
+
+        context['institution'] = institution.id
+        context['children'] = children
+        context['parents'] = parents
+        return context
+
     def get_queryset(self):
         include_history = self.request.query_params.get('history', None)
-        institution_id = self.kwargs['institution']
+        institution = Institution.objects.get(pk=self.kwargs['institution'])
+        institution_ids = [institution.id]
+
+        # Add Children
+        for inst in institution.relationship_parent.all():
+            institution_ids.append(inst.institution_child_id)
+
+        # Add Parents
+        for inst in institution.relationship_child.all():
+            institution_ids.append(inst.institution_parent_id)
 
         if include_history == 'true':
             qs = Programme.objects.filter(
-                Q(report__institutions__id=institution_id) & ~Q(report__flag=3)
+                Q(report__institutions__id__in=institution_ids) & ~Q(report__flag=3)
             )
         else:
             qs = Programme.objects.filter(
-                Q(report__institutions__id=institution_id) & ~Q(report__flag=3) &
+                Q(report__institutions__id__in=institution_ids) & ~Q(report__flag=3) &
                 (
                     Q(report__valid_to__gte=datetime.datetime.now()) | (
                         Q(report__valid_to__isnull=True) &
@@ -52,17 +81,45 @@ class ReportInstitutionListByInstitution(generics.ListAPIView):
     ordering_fields = ('name', 'agency')
     ordering = ('name', 'agency')
 
+    def get_serializer_context(self):
+        context = super(ReportInstitutionListByInstitution, self).get_serializer_context()
+        parents = []
+        children = []
+        institution = Institution.objects.get(pk=self.kwargs['institution'])
+
+        # Children
+        for inst in institution.relationship_parent.all():
+            children.append(inst.institution_child_id)
+
+        # Parents
+        for inst in institution.relationship_child.all():
+            parents.append(inst.institution_parent_id)
+
+        context['institution'] = institution.id
+        context['children'] = children
+        context['parents'] = parents
+        return context
+
     def get_queryset(self):
         include_history = self.request.query_params.get('history', None)
-        institution_id = self.kwargs['institution']
+        institution = Institution.objects.get(pk=self.kwargs['institution'])
+        institution_ids = [institution.id]
+
+        # Add Children
+        for inst in institution.relationship_parent.all():
+            institution_ids.append(inst.institution_child_id)
+
+        # Add Parents
+        for inst in institution.relationship_child.all():
+            institution_ids.append(inst.institution_parent_id)
 
         if include_history == 'true':
             qs = Report.objects.filter(
-                Q(institutions__id=institution_id) & Q(programme__isnull=True)
+                Q(institutions__id__in=institution_ids) & Q(programme__isnull=True)
             )
         else:
             qs = Report.objects.filter(
-                Q(institutions__id=institution_id) & Q(programme__isnull=True) &
+                Q(institutions__id__in=institution_ids) & Q(programme__isnull=True) &
                 (
                     Q(valid_to__gte=datetime.datetime.now()) | (
                         Q(valid_to__isnull=True) &
