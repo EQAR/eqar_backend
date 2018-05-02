343d5c651405d61dea103d664c367b7903bb8d02
JoshBone <jozsef.bone@gmail.com>
2018-05-03 00:17:22 +0200

    Add '/' to url endpoints.

--- adminapi/urls.py
+++ adminapi/urls.py
@@ -9,8 +9,8 @@ from adminapi.views.select_views import CountrySelectList, AgencySelectList, Age
 
 urlpatterns = [
     # Dashboard
-    url(r'^reports_by_agency$', ReportsByAgency.as_view(), name='submit-report'),
-    url(r'^dashboard/badges', DashboardBadgesView.as_view(), name='dashboard-badges'),
+    url(r'^reports_by_agency/$', ReportsByAgency.as_view(), name='submit-report'),
+    url(r'^dashboard/badges/$', DashboardBadgesView.as_view(), name='dashboard-badges'),
 
     # Select Endpoints
     url(r'^select/agency/$', AgencySelectList.as_view(), name='agency-select'),
@@ -19,13 +19,13 @@ urlpatterns = [
     url(r'^select/agency_esg_activity/(?P<pk>[0-9]+)/$', AgencyESGActivitySelectList.as_view(),
         name='agency-activity-select'),
     url(r'^select/country/$', CountrySelectList.as_view(), name='country-select'),
-    url(r'^select/language', LanguageSelectList.as_view(), name='language-select'),
-    url(r'^select/association', AssociationSelectList.as_view(), name='association-select'),
-    url(r'^select/eqar_decision_type', EQARDecisionTypeSelectList.as_view(), name='decision-select'),
-    url(r'^select/identifier_resource', IdentifierResourceSelectList.as_view(),
+    url(r'^select/language/$', LanguageSelectList.as_view(), name='language-select'),
+    url(r'^select/association/$', AssociationSelectList.as_view(), name='association-select'),
+    url(r'^select/eqar_decision_type/$', EQARDecisionTypeSelectList.as_view(), name='decision-select'),
+    url(r'^select/identifier_resource/$', IdentifierResourceSelectList.as_view(),
         name='identifier-resource-select'),
-    url(r'^select/permission_type', PermissionTypeSelectList.as_view(), name='permission-type-select'),
-    url(r'^select/qf_ehea_level', QFEHEALevelSelectList.as_view(), name='qf_ehea_level-select'),
+    url(r'^select/permission_type/$', PermissionTypeSelectList.as_view(), name='permission-type-select'),
+    url(r'^select/qf_ehea_level/$', QFEHEALevelSelectList.as_view(), name='qf_ehea_level-select'),
 
     url(r'^select/institutions/$', InstitutionSelectList.as_view(), name='institution-select'),
     url(r'^select/institutions/country/$', InstitutionCountrySelectList.as_view(), name='institution-country-select'),
