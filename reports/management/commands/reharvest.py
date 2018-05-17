689727b0c6af5c6439952d994f79bb0f1d7c98a6
JoshBone <jozsef.bone@gmail.com>
2018-05-17 20:41:34 +0200

    Make reharvest command for report reharvesting...

---
+++ reports/management/commands/reharvest.py
@@ -0,0 +1,28 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management import BaseCommand, CommandError
+
+from agencies.models import Agency
+from reports.models import Report
+from submissionapi.tasks import download_file
+
+
+class Command(BaseCommand):
+    help = 'Reharvest files.'
+
+    def add_arguments(self, parser):
+        parser.add_argument('--agency', dest='agency_acronym', help='The acronym of the Agency.')
+
+    def handle(self, *args, **options):
+        if not options['agency_acronym']:
+            raise CommandError('--agency parameter should be set.')
+
+        try:
+            agency = Agency.objects.get(acronym_primary=options['agency_acronym'])
+        except ObjectDoesNotExist:
+            raise CommandError('Agency "%s" does not exist' % options['agency_acronym'])
+
+        reports = Report.objects.filter(agency=agency)
+
+        for report in reports:
+            for rf in report.reportfile_set:
+                download_file.delay(rf.file_original_location, rf.id, agency.acronym_primary)
