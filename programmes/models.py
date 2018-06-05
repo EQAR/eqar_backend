40a19e353119076dba314bc0e5af5e683514cd7a
JoshBone <jozsef.bone@gmail.com>
2018-06-05 11:16:07 +0200

    Change the length of the NQF level field.

--- programmes/models.py
+++ programmes/models.py
@@ -8,7 +8,7 @@ class Programme(models.Model):
     id = models.AutoField(primary_key=True)
     report = models.ForeignKey('reports.Report', on_delete=models.CASCADE)
     name_primary = models.CharField(max_length=255, blank=True)
-    nqf_level = models.CharField(max_length=10, blank=True)
+    nqf_level = models.CharField(max_length=255, blank=True)
     qf_ehea_level = models.ForeignKey('lists.QFEHEALevel', on_delete=models.SET_NULL, blank=True, null=True)
     countries = models.ManyToManyField('countries.Country', blank=True)
 
