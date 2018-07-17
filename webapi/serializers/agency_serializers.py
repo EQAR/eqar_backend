2ba1de91dafb7adf41f339677c0dcf1ceef408d3
JoshBone <jozsef.bone@gmail.com>
2018-07-17 20:59:01 +0200

    Fix #234 - Added 'reports_link' to the Agency detail endpoint.

--- webapi/serializers/agency_serializers.py
+++ webapi/serializers/agency_serializers.py
@@ -102,7 +102,8 @@ class AgencyDetailSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Agency
-        fields = ('id', 'deqar_id', 'names', 'contact_person', 'registration_start', 'registration_valid_to', 'registration_note',
+        fields = ('id', 'deqar_id', 'names', 'contact_person', 'registration_start', 'registration_valid_to',
+                  'registration_note', 'reports_link',
                   'phone_numbers', 'address', 'country', 'emails', 'website_link', 'logo',
                   'report_count', 'activities', 'associations', 'decisions', 'specialisation_note',
                   'description_note', 'geographical_focus', 'historical_data',)
