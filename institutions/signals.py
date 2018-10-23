ba87f339b2e51adf46cd97bb584363bf0b7a1505
JoshBone <jozsef.bone@gmail.com>
2018-10-23 22:45:24 +0200

    Fix #251 - Add post_delete signal as well.

--- institutions/signals.py
+++ institutions/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from institutions.models import InstitutionName
 
 
-@receiver(post_save, sender=InstitutionName)
+@receiver([post_save, post_delete], sender=InstitutionName)
 def set_institution_properties(sender, instance, **kwargs):
     institution = instance.institution
     institution.set_primary_name()
