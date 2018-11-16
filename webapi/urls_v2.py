d050157a5f7fdfe9637848aa427929b2ba4cb646
JoshBone <jozsef.bone@gmail.com>
2018-11-16 15:15:48 +0100

    Fix #223 - Introducing the proposed display of report names.

--- webapi/urls_v2.py
+++ webapi/urls_v2.py
@@ -3,6 +3,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from webapi.views.institution_v2_views import *
+from webapi.views.report_v2_views import *
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -21,6 +22,12 @@ app_name = 'webapi'
 
 urlpatterns = [
     url(r'^browse/institutions/$', InstitutionList.as_view(), name='institution-list'),
+
+    url(r'^browse/reports/programme/by-institution/(?P<institution>[0-9]+)/$',
+        ReportProgrammeListByInstitution.as_view(), name='programme-report-list-by-institution'),
+    url(r'^browse/reports/institutional/by-institution/(?P<institution>[0-9]+)/$',
+        ReportInstitutionListByInstitution.as_view(), name='institutional-report-list-by-institution'),
+
 
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=None), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
