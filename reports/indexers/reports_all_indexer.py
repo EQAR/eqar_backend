26b295e1dc17eb083d99d5c31d6c114bc3203f23
József Gábor Bóné <jozsef.bone@gmail.com>
2019-02-18 22:53:48 +0100

    Fix report_all_indexer country part.

--- reports/indexers/reports_all_indexer.py
+++ reports/indexers/reports_all_indexer.py
@@ -98,7 +98,7 @@ class ReportsAllIndexer:
                 self.doc['programme_name'].append(pname.name.strip())
 
             for c in programme.countries.all():
-                self.doc['country'].append(c.country.name_english)
+                self.doc['country'].append(c.name_english)
 
         institutions = "; ".join(institutions)
         programmes = " / ".join(programmes)
