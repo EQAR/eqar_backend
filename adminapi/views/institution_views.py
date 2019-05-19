f3f8bf555f690ef9b630aad11ba2f90e34d11560
József Gábor Bóné <jozsef.bone@gmail.com>
2019-05-19 21:28:37 +0200

    feat(AdminAPI): Endpoints for institution retrievals and creation with Swagger doc.

--- adminapi/views/institution_views.py
+++ adminapi/views/institution_views.py
@@ -1,3 +1,4 @@
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import CreateAPIView
 from drf_rw_serializers.generics import RetrieveUpdateAPIView
 
@@ -9,6 +10,14 @@ class InstitutionDetail(RetrieveUpdateAPIView):
     queryset = Institution.objects.all()
     read_serializer_class = InstitutionReadSerializer
     write_serializer_class = InstitutionWriteSerializer
+
+    @swagger_auto_schema(responses={'200': InstitutionReadSerializer})
+    def get(self, request, *args, **kwargs):
+        return super(InstitutionDetail, self).get(request, *args, **kwargs)
+
+    @swagger_auto_schema(request_body=InstitutionWriteSerializer)
+    def put(self, request, *args, **kwargs):
+        return super(InstitutionDetail, self).get(request, *args, **kwargs)
 
 
 class InstitutionCreate(CreateAPIView):
