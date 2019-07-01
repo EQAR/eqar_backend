ad18c7b6214c3351bfcf02f584d3c902f86ab437
József Gábor Bóné <jozsef.bone@gmail.com>
2019-07-01 19:48:15 +0200

    Fix #292 - Adding signals to reindex institutions upon save - It's sufficient to have only parent records.

--- agencies/signals.py
+++ agencies/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from agencies.models import Agency, AgencyName, AgencyNameVersion, AgencyFocusCountry, AgencyESGActivity
+from agencies.models import Agency
 from institutions.tasks import index_institution
 
 
@@ -9,28 +9,3 @@ from institutions.tasks import index_institution
 def do_index_institutions_upon_agency_save(sender, instance, **kwargs):
     for institution in instance.reports.institutions:
         index_institution.delay(institution.id)
-        
-        
-@receiver([post_save, post_delete], sender=AgencyName)
-def do_index_institutions_upon_agency_name_save(sender, instance, **kwargs):
-    for institution in instance.agency.reports.institutions:
-        index_institution.delay(institution.id)
-
-
-@receiver([post_save, post_delete], sender=AgencyNameVersion)
-def do_index_institutions_upon_agency_name_version_save(sender, instance, **kwargs):
-    for institution in instance.agency_name.agency.reports.institutions:
-        index_institution.delay(institution.id)
-
-
-@receiver([post_save, post_delete], sender=AgencyFocusCountry)
-def do_index_institutions_upon_agency_focus_country_save(sender, instance, **kwargs):
-    for institution in instance.agency.reports.institutions:
-        index_institution.delay(institution.id)
-
-
-@receiver([post_save, post_delete], sender=AgencyESGActivity)
-def do_index_institutions_upon_agency_esg_activity_save(sender, instance, **kwargs):
-    for institution in instance.agency.reports.institutions:
-        index_institution.delay(institution.id)
-
