ebace2cee57a92810df3b33146195aaf780c903d
József Gábor Bóné <jozsef.bone@gmail.com>
2019-07-11 08:58:22 +0200

    fix(adminAPI): Add institutionUpdateLog to 'PUT' method of the institution record management.

--- adminapi/views/institution_views.py
+++ adminapi/views/institution_views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import CreateAPIView
 from drf_rw_serializers.generics import RetrieveUpdateAPIView
 
 from adminapi.serializers.institution_serializers import InstitutionReadSerializer, InstitutionWriteSerializer
-from institutions.models import Institution
+from institutions.models import Institution, InstitutionUpdateLog
 
 
 class InstitutionDetail(RetrieveUpdateAPIView):
@@ -17,6 +17,16 @@ class InstitutionDetail(RetrieveUpdateAPIView):
 
     @swagger_auto_schema(request_body=InstitutionWriteSerializer)
     def put(self, request, *args, **kwargs):
+        institution = Institution.objects.get(id=kwargs.get('pk'))
+
+        submit_comment = request.data.get('submit_comment', None)
+        if submit_comment:
+            InstitutionUpdateLog.objects.create(
+                institution=institution,
+                note=submit_comment,
+                updated_by=request.user
+            )
+        institution.save()
         return super(InstitutionDetail, self).put(request, *args, **kwargs)
 
 
