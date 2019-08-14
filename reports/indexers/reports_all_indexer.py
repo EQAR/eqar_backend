c633d58c8aa9d6ab044d608c45466276f8c0e0d5
József Gábor Bóné <jozsef.bone@gmail.com>
2019-08-14 19:35:33 +0200

    feat(ReportIndexer): Add 'id_sort' for index.

--- reports/indexers/reports_all_indexer.py
+++ reports/indexers/reports_all_indexer.py
@@ -48,6 +48,7 @@ class ReportsAllIndexer:
 
     def _index_report(self):
         self.doc['id'] = self.report.id
+        self.doc['id_sort'] = self.report.id
         self.doc['local_id'] = self.report.local_identifier
         self.doc['agency'] = self.report.agency.acronym_primary
         self.doc['activity'] = self.report.agency_esg_activity.activity
