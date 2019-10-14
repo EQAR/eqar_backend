0cb9c2aaae0c608b6117066743821154392c52ab
József Gábor Bóné <jozsef.bone@gmail.com>
2019-10-14 20:41:23 +0200

    fix(ReportIndexer): Remove not used indexer commands.

--- reports/management/commands/index_reports.py
+++ reports/management/commands/index_reports.py
@@ -2,7 +2,6 @@ from django.core.management import BaseCommand
 
 from reports.indexers.reports_indexer import ReportsIndexer
 from reports.models import Report
-from reports.indexers.reports_all_indexer import ReportsAllIndexer
 
 
 class Command(BaseCommand):
