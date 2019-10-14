0c8dff05566b349cd4a60e21b40692bbb2edc7bc
József Gábor Bóné <jozsef.bone@gmail.com>
2019-10-14 20:56:23 +0200

    fix(IndexerCommands): Remove existing entries from the index.

--- agencies/management/commands/index_agencies.py
+++ agencies/management/commands/index_agencies.py
@@ -1,4 +1,6 @@
+import pysolr
 from django.core.management import BaseCommand
+from django.conf import settings
 
 from agencies.indexers.agency_indexer import AgencyIndexer
 from agencies.models import Agency
@@ -8,6 +10,11 @@ class Command(BaseCommand):
     help = 'Index Agency records.'
 
     def handle(self, *args, **options):
+        solr_core = getattr(settings, "SOLR_CORE_AGENCIES", "deqar-agencies")
+        solr_url = "%s/%s" % (getattr(settings, "SOLR_URL", "http://localhost:8983/solr"), solr_core)
+        solr = pysolr.Solr(solr_url)
+        solr.delete(q='*:*', commit=True)
+
         for agency in Agency.objects.iterator():
             indexer = AgencyIndexer(agency)
             indexer.index()
--- reports/management/commands/index_reports.py
+++ reports/management/commands/index_reports.py
@@ -1,4 +1,6 @@
+import pysolr
 from django.core.management import BaseCommand
+from django.conf import settings
 
 from reports.indexers.reports_indexer import ReportsIndexer
 from reports.models import Report
@@ -8,6 +10,11 @@ class Command(BaseCommand):
     help = 'Index Report records.'
 
     def handle(self, *args, **options):
+        solr_core = getattr(settings, "SOLR_CORE_REPORTS", "deqar-reports")
+        solr_url = "%s/%s" % (getattr(settings, "SOLR_URL", "http://localhost:8983/solr"), solr_core)
+        solr = pysolr.Solr(solr_url)
+        solr.delete(q='*:*', commit=True)
+
         for report in Report.objects.iterator():
             indexer = ReportsIndexer(report)
             indexer.index()
