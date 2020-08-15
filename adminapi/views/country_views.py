3af8d82dcf2db1041ec763d439431418e8f4a3b6
József Gábor Bóné <jozsef.bone@gmail.com>
2020-08-15 16:26:45 +0200

    fix(adminAPI): Set default ordering of countries to 'name_english' when ordering by ehea membership.

--- adminapi/views/country_views.py
+++ adminapi/views/country_views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics
-from rest_framework.filters import OrderingFilter
+from rest_framework.filters import OrderingFilter, BaseFilterBackend
 from rest_framework import filters
 from drf_rw_serializers.generics import RetrieveUpdateAPIView
 
@@ -10,9 +10,25 @@ from adminapi.serializers.country_serializer import CountryListSerializer, Count
 from countries.models import Country, CountryUpdateLog
 
 
+class CountryOrderingFilter(OrderingFilter):
+    def get_ordering(self, request, queryset, view):
+        params = request.query_params.get(self.ordering_param)
+        if params:
+            fields = [param.strip() for param in params.split(',')]
+            ordering = self.remove_invalid_fields(queryset, fields, view, request)
+            if ordering:
+                for order in ordering:
+                    if 'ehea_is_member' in order:
+                        ordering.append('name_english')
+                return ordering
+
+        # No ordering was included, or all the ordering fields were invalid
+        return self.get_default_ordering(view)
+
+
 class CountryList(generics.ListCreateAPIView):
     serializer_class = CountryListSerializer
-    filter_backends = (OrderingFilter, filters.SearchFilter, DjangoFilterBackend)
+    filter_backends = (CountryOrderingFilter, filters.SearchFilter, DjangoFilterBackend)
     filterset_fields = ['ehea_is_member', 'external_QAA_is_permitted', 'european_approach_is_permitted',
                         'ehea_key_commitment']
     ordering = ['name_english', 'iso_3166_alpha2', 'iso_3166_alpha3', 'ehea_is_member']
