93a075ca617ef187b81fe01d9f9152180230ce62
József Gábor Bóné <jozsef.bone@gmail.com>
2021-07-14 22:34:02 +0200

    feat(ebsi): Add placeholder.

--- connectapi/urls.py
+++ connectapi/urls.py
@@ -3,6 +3,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
+from connectapi.letstrust.views import VCIssue
 from connectapi.views import InstitutionDEQARConnectList, AgencyActivityDEQARConnectList, AccreditationXMLView
 
 schema_view = get_schema_view(
@@ -25,7 +26,11 @@ urlpatterns = [
     url(r'^institutions/$', InstitutionDEQARConnectList.as_view(), name='institution-deqar-connect-list'),
     url(r'^activities/$', AgencyActivityDEQARConnectList.as_view(), name='agency-activity-deqar-connect-list'),
 
+    # Europass endpoints
     url(r'^europass/accreditations/(?P<country_code>[a-zA-Z]{3})$', AccreditationXMLView.as_view(), name='europass-accreditations'),
+
+    # Let's Trust endpoints
+    url(r'^letstrust/vc/issue/(?P<report_id>[0-9]+)$', VCIssue.as_view(), name='letstrust-vc-issue'),
 
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=None), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
