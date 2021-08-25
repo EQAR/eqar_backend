77c9668aa4e3b7fb4b991c718b3705abd37060e0
Josh <jozsef.bone@gmail.com>
2021-08-25 14:25:35 +0200

    fix(AgencyESGActivity): Fix #395 - Reindex reports when ESGActivityName changes

--- agencies/apps.py
+++ agencies/apps.py
@@ -5,3 +5,4 @@ class AgenciesConfig(AppConfig):
     name = 'agencies'
     from agencies.signals import do_index_institutions_upon_agency_save
     from agencies.signals import do_index_agencies
+    from agencies.signals import do_index_reports_upon_activity_name_change
