87228c244d98c27a2a8fa2442e7c37b0525a3fdc
Josh <jozsef.bone@gmail.com>
2022-05-29 21:14:14 +0200

    Fix(AgencyIndexer): Fix #395 Correcting the import of signals

--- agencies/apps.py
+++ agencies/apps.py
@@ -3,6 +3,9 @@ from django.apps import AppConfig
 
 class AgenciesConfig(AppConfig):
     name = 'agencies'
-    from agencies.signals import do_index_institutions_upon_agency_save
-    from agencies.signals import do_index_agencies
-    from agencies.signals import do_index_reports_upon_activity_name_change
+
+    def ready(self):
+        super(AgenciesConfig, self).ready()
+        from agencies.signals import do_index_institutions_upon_agency_save
+        from agencies.signals import do_index_agencies
+        from agencies.signals import do_index_reports_upon_activity_name_change
