7010f59d246e652c00e05d4fd32747ba2e04ae24
Josh <jozsef.bone@gmail.com>
2022-06-20 22:39:09 +0200

    fix(signals): Better performance with agency indexing.

--- agencies/signals.py
+++ agencies/signals.py
@@ -1,17 +1,20 @@
+import time
+
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from agencies.models import Agency, AgencyESGActivity
 from agencies.tasks import index_agency
+from institutions.models import Institution
 from institutions.tasks import index_institution
 from reports.tasks import index_report
 
 
 @receiver([post_save, post_delete], sender=Agency)
 def do_index_institutions_upon_agency_save(sender, instance, **kwargs):
-    for report in instance.report_set.iterator():
-        for institution in report.institutions.iterator():
-            index_institution.delay(institution.id)
+    institutions = Institution.objects.filter(reports__agency=instance)
+    for inst in institutions.all():
+        index_institution.delay(inst.id)
 
 
 @receiver([post_save], sender=Agency)
@@ -23,3 +26,4 @@ def do_index_agencies(sender, instance, **kwargs):
 def do_index_reports_upon_activity_name_change(sender, instance, **kwargs):
     for report in instance.report_set.iterator():
         index_report.delay(report.id)
+
