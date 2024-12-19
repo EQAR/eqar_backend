25146e9e8e8e76b6674793bbe4e1602ff68cc8cc
Josh <jozsef.bone@gmail.com>
2024-12-19 22:09:03 +0100

    Update test_indexer.py

--- agencies/tests/test_indexer.py
+++ agencies/tests/test_indexer.py
@@ -2,35 +2,29 @@ import unittest
 from unittest.mock import patch, MagicMock
 from agencies.indexers.agency_indexer import AgencyIndexer
 from agencies.models import Agency
-from countries.models import Country
 
 class TestAgencyIndexer(unittest.TestCase):
+    fixtures = ['agency_demo_01', 'country']
 
     def setUp(self):
-        self.country = Country(id=1, name_english='Germany')
-        self.agency = Agency(
-            id=1,
-            acronym_primary='ACQUIN',
-            name_primary='Accreditation, Certification and Quality Assurance Institute',
-            country=self.country,
-            registration_start='2020-01-01',
-            registration_valid_to='2025-01-01'
-        )
-        self.indexer = AgencyIndexer(self.agency)
+        agency = Agency.objects.get(id=5)
+        self.indexer = AgencyIndexer(agency)
 
     @patch('agencies.indexers.agency_indexer.pysolr.Solr.add')
     def test_index(self, mock_solr_add):
         self.indexer.index()
         mock_solr_add.assert_called_once()
         self.assertIn('id', self.indexer.doc)
-        self.assertEqual(self.indexer.doc['id'], 1)
+        self.assertEqual(self.indexer.doc['id'], 5)
 
     def test_index_agency(self):
         self.indexer._index_agency()
-        self.assertEqual(self.indexer.doc['id'], 1)
+        self.assertEqual(self.indexer.doc['id'], 5)
         self.assertEqual(self.indexer.doc['acronym'], 'ACQUIN')
         self.assertEqual(self.indexer.doc['name'], 'Accreditation, Certification and Quality Assurance Institute')
         self.assertEqual(self.indexer.doc['country'], 'Germany')
+        self.assertEqual(self.indexer.doc['name_version_search'][1], 'Akkreditierungs-, Certifizierungs- und Qualitätssicherungsinstitut e.V.')
+        self.assertIn('Bulgaria', self.indexer.doc['focus_country_facet'])
 
     def test_remove_duplicates(self):
         self.indexer.doc['name_version_search'] = ['name1', 'name1', 'name2']
