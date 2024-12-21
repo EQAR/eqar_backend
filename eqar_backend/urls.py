991b31dd9a7cc7781ad74f4664446f37027756af
Josh <jozsef.bone@gmail.com>
2024-12-21 18:56:13 +0100

    Update urls.py

--- eqar_backend/urls.py
+++ eqar_backend/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^webapi/v2/', include('webapi.v2.urls', namespace='webapi-v2')),
 
     url(r'^submissionapi/v1/', include('submissionapi.v1.urls', namespace='submissionapi-v1')),
+    url(r'^submissionapi/v2/', include('submissionapi.v2.urls', namespace='submissionapi-v2')),
 
     url(r'^adminapi/v1/', include('adminapi.urls', namespace='adminapi-v1')),
     url(r'^connectapi/v1/', include('connectapi.urls', namespace='connectapi-v1')),
