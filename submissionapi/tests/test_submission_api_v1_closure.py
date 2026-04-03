198b90e5eb73ca5a96978c34412ddc81073dfe0b
Josh <jozsef.bone@gmail.com>
2026-04-03 15:13:57 +0200

    Update test_submission_api_v1_closure.py

--- submissionapi/tests/test_submission_api_v1_closure.py
+++ submissionapi/tests/test_submission_api_v1_closure.py
@@ -2,28 +2,10 @@ from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from accounts.models import DEQARProfile
-from agencies.models import SubmittingAgency
 from submissionapi.v1.closure import SUBMISSION_V1_CLOSED_MESSAGE
 
 
 class SubmissionAPIV1ReportTest(APITestCase):
-    fixtures = [
-        'country_qa_requirement_type', 'country', 'qf_ehea_level', 'eqar_decision_type', 'language',
-        'agency_activity_type', 'agency_focus', 'identifier_resource', 'flag', 'permission_type', 'degree_outcome',
-        'agency_historical_field',
-        'agency_demo_01', 'agency_demo_02', 'association',
-        'submitting_agency_demo',
-        'institution_historical_field',
-        'institution_demo_01', 'institution_demo_02', 'institution_demo_03',
-        'programme_demo_01', 'programme_demo_02', 'programme_demo_03',
-        'programme_demo_04', 'programme_demo_05', 'programme_demo_06',
-        'programme_demo_07', 'programme_demo_08', 'programme_demo_09',
-        'programme_demo_10', 'programme_demo_11', 'programme_demo_12',
-        'report_decision', 'report_status',
-        'users', 'report_demo_01'
-    ]
-
     def setUp(self):
         self.valid_data = {
             "agency": "ACQUIN",
@@ -62,9 +44,6 @@ class SubmissionAPIV1ReportTest(APITestCase):
                                              password='testpassword')
         self.user.save()
         self.token = Token.objects.get(user__username='testuser')
-        submitting_agency = SubmittingAgency.objects.get(pk=1)
-        self.deqar_profile = DEQARProfile.objects.create(user=self.user, submitting_agency=submitting_agency)
-        self.deqar_profile.save()
 
     def test_closure_submission(self):
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token.key)
@@ -77,7 +56,6 @@ class SubmissionAPIV1ReportTest(APITestCase):
         self.assertEqual(response.data['detail'], SUBMISSION_V1_CLOSED_MESSAGE)
 
     def test_closure_delete(self):
-        DEQARProfile.objects.create(user=self.user, submitting_agency_id=1)
         self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token.key)
         response = self.client.delete('/submissionapi/v1/delete/report/2/')
         self.assertEqual(response.status_code, 410)
